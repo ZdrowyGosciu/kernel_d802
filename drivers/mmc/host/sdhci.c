b2d1a66f0e9c5519d53dbea1a92adcf01a10df5b
Kevin Liu <kliu5@marvell.com>
2013-01-05 17:18:28 +0800

    mmc: sdhci: disable interrupt before free_irq
    Current code missed disabling interrupts before free irq which is shared.
    
    Notice below comments for function free_irq (kernel/irq/manage.c):
    On a shared IRQ the caller must ensure the interrupt is disabled
    on the card it drives before calling this function.
    
    Original code has below issue during suspend/resume when multiple SD
    hosts share the same IRQ:
    1. Assume there are two hosts (host1 for emmc while host2 for sd) share
    the same mmc irq.
    2. When system suspend, host2 will be suspended before host1.
    So the sequence is below:
    	step1: irq handler for host2 removed ->
    	step2: irq handler for host1 removed and irq disabled ->
    	... system suspended ...
    	... system resumed ...
    	step3: irq enabled and the irq handler for host1 restored ->
    	step4: irq handler for host2 restored
    3. So there is the buggy time slot that the irq is enabled but the irq
    handler for host2 is removed. Then host2 interrupt can be triggered
    but can't be handled at that moment.
    
    Signed-off-by: Jialing Fu <jlfu@marvell.com>
    Signed-off-by: Kevin Liu <kliu5@marvell.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -2991,6 +2991,7 @@ int sdhci_suspend_host(struct sdhci_host *host)
 		return ret;
 	}
 
+	sdhci_mask_irqs(host, SDHCI_INT_ALL_MASK);
 	free_irq(host->irq, host);
 
 	return ret;
@@ -3672,6 +3673,7 @@ int sdhci_add_host(struct sdhci_host *host)
 #ifdef SDHCI_USE_LEDS_CLASS
 reset:
 	sdhci_reset(host, SDHCI_RESET_ALL);
+	sdhci_mask_irqs(host, SDHCI_INT_ALL_MASK);
 	free_irq(host->irq, host);
 #endif
 untasklet:
@@ -3716,6 +3718,7 @@ void sdhci_remove_host(struct sdhci_host *host, int dead)
 	if (!dead)
 		sdhci_reset(host, SDHCI_RESET_ALL);
 
+	sdhci_mask_irqs(host, SDHCI_INT_ALL_MASK);
 	free_irq(host->irq, host);
 
 	del_timer_sync(&host->timer);
