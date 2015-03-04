e58d63b39abe65721b9b766abdfbd1a63e646567
Pranav Vashi <neobuddy89@gmail.com>
2015-03-04 17:52:58 +0100

    drivers: Introduce Notifier Driver
    Signed-off-by: fechinator <fechter73@googlemail.com>
    
    Conflicts:
    	drivers/input/touchscreen/Kconfig

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -23,6 +23,11 @@
 #include <linux/regulator/consumer.h>
 #include <mach/board_lge.h>
 
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
+
+
 #include "mdss.h"
 #include "mdss_panel.h"
 #include "mdss_dsi.h"
@@ -1384,6 +1389,10 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 							pdata);
 		if (ctrl_pdata->on_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_unblank(pdata);
+#ifdef CONFIG_STATE_NOTIFIER
+		if (!use_fb_notifier)
+			state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
+#endif
 		break;
 	case MDSS_EVENT_PANEL_ON:
 		ctrl_pdata->ctrl_state |= CTRL_STATE_MDP_ACTIVE;
@@ -1405,6 +1414,10 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
         }
 #endif
 		break;
+#ifdef CONFIG_STATE_NOTIFIER
+		if (!use_fb_notifier)
+			state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
+#endif
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_blank(pdata);
