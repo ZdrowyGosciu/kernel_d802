4152a06d4e70f370b78c4fdb34c7c9e0162c7e68
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2015-06-28 19:41:01 +0200

    Revert "Revert powersuspend: add power_suspended boolean for global access"
    Depends on 54b529ac40005c2321f1adf5d885eae3215f79dd.

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -66,9 +66,6 @@ static DEFINE_SPINLOCK(tz_lock);
 
 static bool suspended;
 
-/* Boolean to detect if panel has gone off */
-static bool power_suspended = false;
-
 /* Trap into the TrustZone, and call funcs there. */
 static int __secure_tz_entry2(u32 cmd, u32 val1, u32 val2)
 {
@@ -408,26 +405,8 @@ static struct devfreq_governor msm_adreno_tz = {
 	.event_handler = tz_handler,
 };
 
-static void tz_early_suspend(struct power_suspend *handler)
-{
-	power_suspended = true;
-	return;
-}
-
-static void tz_late_resume(struct power_suspend *handler)
-{
-	power_suspended = false;
-	return;
-}
-
-static struct power_suspend tz_power_suspend = {
-	.suspend = tz_early_suspend,
-	.resume = tz_late_resume,
-};
-
 static int __init msm_adreno_tz_init(void)
 {
-	register_power_suspend(&tz_power_suspend);
 	return devfreq_add_governor(&msm_adreno_tz);
 }
 subsys_initcall(msm_adreno_tz_init);
