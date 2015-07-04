d7ec1f4271fca1c6a786395dadbe65fe4f3f6998
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2015-07-05 01:43:02 +0200

    smb349_charger: Remove Fast Charge (for now)
    This reverts commit 0e5f0cabcbe343c8624dd90bd8b4e44b7a991066.
    
    Conflicts:
    	arch/arm/configs/d802_defconfig

--- drivers/usb/dwc3/dwc3_otg.h
+++ drivers/usb/dwc3/dwc3_otg.h
@@ -33,7 +33,6 @@
 #endif
 
 #define DWC3_IDEV_CHG_MAX 1500
-#define DWC3_IDEV_CHG_MIN 500
 
 #ifdef CONFIG_LGE_PM
 #define DWC3_IDEV_CHG_PROPRIETARY_MAX 1000
@@ -125,7 +124,6 @@ struct dwc3_charger {
 
 /* for external charger driver */
 extern int dwc3_set_charger(struct usb_otg *otg, struct dwc3_charger *charger);
-extern int smb349_thermal_mitigation_update(int value);
 
 enum dwc3_ext_events {
 	DWC3_EVENT_NONE = 0,		/* no change event */
