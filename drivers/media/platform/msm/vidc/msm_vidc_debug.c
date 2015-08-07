e966d622be1be51167afaea25a77f2eac0aac42e
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2015-08-07 10:08:22 +0200

    drivers: media: vidc: Reduce debug messages

--- drivers/media/platform/msm/vidc/msm_vidc_debug.c
+++ drivers/media/platform/msm/vidc/msm_vidc_debug.c
@@ -14,8 +14,8 @@
 #include "msm_vidc_debug.h"
 #include "vidc_hfi_api.h"
 
-#define MAX_DBG_BUF_SIZE 4096
-int msm_vidc_debug = VIDC_ERR | VIDC_WARN;
+#define MAX_DBG_BUF_SIZE 1024
+int msm_vidc_debug = VIDC_ERR;
 int msm_vidc_debug_out = VIDC_OUT_PRINTK;
 int msm_fw_debug = 0x18;
 int msm_fw_debug_mode = 0x1;
