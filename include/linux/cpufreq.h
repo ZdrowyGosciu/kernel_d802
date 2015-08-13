23e39f9d07478ef804d1d54bec485097ba6c88c3
maxwen <max.weninger@gmail.com>
2015-08-13 12:01:07 +0200

    cpufreq: Introduce SmartMax GOV
    Included :
    * Initial commit https://github.com/sultanxda/android_kernel_oneplus_msm8974/commit/9542afcea536f84ac0f562f625906c522d33fcc9
    * Relax defaults https://github.com/sultanxda/android_kernel_oneplus_msm8974/commit/d990f0025028bc38cea0dca52264e74eb7b82702
    * Boost https://github.com/sultanxda/android_kernel_oneplus_msm8974/commit/4543035c6c6e854ea3deefd8526a47781bc852e8
    * CPU IDLE time update https://github.com/sultanxda/android_kernel_oneplus_msm8974/commit/0bb74eb0f035c072d5c292cff31c119b3f47632a
    * Removed boosting https://github.com/sultanxda/android_kernel_oneplus_msm8974/commit/5d6784f03f3712e5fc06bd1747545c67c3b2005b
    * Fix NPE https://github.com/sultanxda/android_kernel_oneplus_msm8974/commit/eacab0716ea7a1ac0d8337a7e16535e70dfffe93
    
    Signed-off-by: Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -380,6 +380,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ARTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_arteractive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_arteractive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
 #endif
 
 
